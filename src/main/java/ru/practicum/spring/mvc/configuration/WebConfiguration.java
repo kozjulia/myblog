2ed858c813c51af715d5352612d7ed24e14318bf
Julia Kozlova <kozjuliajulia@yandex.ru>
2025-05-09 15:31:42 +0300

    add configurations

---
+++ src/main/java/ru/practicum/spring/mvc/configuration/WebConfiguration.java
@@ -0,0 +1,12 @@
+package ru.practicum.spring.mvc.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"ru.practicum.spring.mvc"})
+public class WebConfiguration {
+
+}
